496754e6d61b67e8210a25cdb15c5fd93974d227
KostyaF1 <k_moroz@hotmail.com>
2018-11-30 14:19:44 +0200

    First commit

---
+++ setup/setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "*****************************************"
+echo "Starting the replica set"
+echo "*****************************************"
+
+sleep 10 | echo "Sleeping"
+
+mongo mongodb://mongo-rs0-1:27017 replicaSet.js
+
